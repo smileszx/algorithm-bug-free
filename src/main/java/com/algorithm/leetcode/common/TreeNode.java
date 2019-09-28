77df1f1402def03efb088be6fd981e61a5dde338
smile <smile.szx@outlook.com>
2019-09-28 23:39:15 +0800

    init algorithm project

---
+++ src/main/java/com/algorithm/leetcode/common/TreeNode.java
@@ -0,0 +1,17 @@
+package com.algorithm.leetcode.common;
+
+/**
+ * @Description
+ * 
+ * @Author victor su
+ * @Date 2019/8/11 23:38
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        this.val = x;
+    }
+}
